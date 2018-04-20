47e50ca44ac1e0bc4c1e6c1dde881f288406c208
FromSi <fromsi665@gmail.com>
2018-04-20 21:16:53 +0600

    Add PUT putNick

--- src/main/java/kz/sgq/jdbc/JDBCPUT.java
+++ src/main/java/kz/sgq/jdbc/JDBCPUT.java
@@ -1,4 +1,51 @@
 package kz.sgq.jdbc;
+import spark.Request;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
 public class JDBCPUT {
+    private URI dbUri = new URI(System.getenv("JAWSDB_URL"));
+    private final String url = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+    private final String login = dbUri.getUserInfo().split(":")[0];
+    private final String password = dbUri.getUserInfo().split(":")[1];
+    private Connection connection;
+    private Statement statement;
+
+    public JDBCPUT() throws URISyntaxException {
+        try {
+            connection = DriverManager.getConnection(url, login, password);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Error SQL Connecting");
+        }
+    }
+
+    public String putNick(Request request) {
+        boolean check = false;
+        try {
+            if (request.queryParams("idusers") != null &&
+                    request.queryParams("nick") != null){
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM users WHERE users.idusers=" +
+                        request.queryParams("idusers"));
+                while (resultSet.next()) {
+                    statement.execute("UPDATE users SET nick='" +
+                            request.queryParams("nick") + "' where idusers=" +
+                            request.queryParams("idusers"));
+                    check = true;
+                }
+            }
+        } catch (Exception e) {
+            check = false;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (check)
+            return null;
+        return null;
+    }
 }
