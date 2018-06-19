15c6a29044ae25c785708813f88d04210dac3097
FromSi <fromsi665@gmail.com>
2018-06-19 20:56:52 +0600

    New version site

--- src/main/java/kz/sgq/Main.java
+++ src/main/java/kz/sgq/Main.java
@@ -11,117 +11,21 @@ public class Main {
     public static void main(String[] args){
         port(getHerokuAssignedPort());
         staticFiles.location("/public");
-
-        /**
-         * https://example.com/user ?
-         * & avatar
-         * & nick
-         * & login
-         * & password
-         * & token
-         */
         post("/user", (request, response) -> new JDBCPOST().createUser(request));
-
-        /**
-         * https://example.com/friend ?
-         * & iduser_1
-         * & iduser_2
-         */
         post("/friend", (request, response) -> new JDBCPOST().createFriends(request));
-
-        /**
-         * https://example.com/message ?
-         * & iduser_1
-         * & iduser_2
-         * & content
-         * & time
-         */
         post("/message", (request, response) -> new JDBCPOST().createMS(request));
-
-        /**
-         * https://example.com/nick ?
-         * & iduser
-         * & nick
-         */
         put("/nick", (request, response) -> new JDBCPUT().putNick(request));
-
-        /**
-         * https://example.com/read ?
-         * & idchats
-         * & read
-         */
         put("/read", (request, response) -> new JDBCPUT().putRead(request));
-
-        /**
-         * https://example.com/avatar ?
-         * & iduser
-         * & avatar
-         */
         put("/avatar", (request, response) -> new JDBCPUT().putAvatar(request));
-
-        /**
-         * https://example.com/password ?
-         * & login
-         * & avatar
-         */
         put("/password", (request, response) -> new JDBCPUT().putPassword(request));
-
-        /**
-         * https://example.com/token ?
-         * & iduser
-         * & token
-         */
         put("/token", (request, response) -> new JDBCPUT().putToken(request));
-
-        /**
-         * https://example.com/bio ?
-         * & iduser
-         * & bio
-         */
         put("/bio", (request, response) -> new JDBCPUT().putBio(request));
-
-        /**
-         * https://example.com/friend ?
-         * & iduser
-         */
         get("/friend", (request, response) -> new JDBCGET().printFriends(request));
-
-        /**
-         * https://example.com/profile ?
-         * & iduser
-         */
         get("/profile", (request, response) -> new JDBCGET().printProfile(request));
-
-        /**
-         * https://example.com/login ?
-         * & login
-         * & password
-         */
         get("/login", (request, response) -> new JDBCGET().printLogin(request));
-
-        /**
-         * https://example.com/chats ?
-         * & iduser
-         */
         get("/chats", (request, response) -> new JDBCGET().printChats(request));
-
-        /**
-         * https://example.com/message ?
-         * & iduser
-         */
         get("/message", (request, response) -> new JDBCGET().printMessage(request));
-
-        /**
-         * https://example.com/friend ?
-         * & idfriends
-         */
         delete("/friend", (request, response) -> new JDBCDELETE().deleteFriend(request));
-
-        /**
-         * https://example.com/message ?
-         * & iduser
-         * & idmessage
-         */
         delete("/message", (request, response) -> new JDBCDELETE().deleteMessage(request));
     }
 
